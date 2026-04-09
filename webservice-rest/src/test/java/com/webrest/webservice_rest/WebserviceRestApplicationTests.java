ffedc56843d11679d505190a66a0b7599707e3bb
Bruno Malagoli <99491888+BrunoCGM@users.noreply.github.com>
2026-04-09 16:15:04 +0000

    feat: criada a  estrutura inicial do projeto Spring Boot - por Bruno M

---
+++ webservice-rest/src/test/java/com/webrest/webservice_rest/WebserviceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webrest.webservice_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
